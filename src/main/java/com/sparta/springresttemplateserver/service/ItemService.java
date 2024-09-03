b68d0473f20f2d7bbf96eb124b71327951b343a0
gminnimk <tvt1245@naver.com>
2024-09-03 21:19:20 +0900

    2-3 : RestTemplate의 Post 요청

--- src/main/java/com/sparta/springresttemplateserver/service/ItemService.java
+++ src/main/java/com/sparta/springresttemplateserver/service/ItemService.java
@@ -65,17 +65,19 @@ public class ItemService {
     /**
      * ✅ 클라이언트가 전달한 쿼리와 요청 본문 데이터를 기반으로 아이템을 생성합니다.
      *
-     *    ➡️ 쿼리와 요청 본문 데이터를 바탕으로 새로운 아이템을 생성하고 반환합니다.
+     * ➡️ 클라이언트의 요청 본문 데이터와 쿼리 파라미터를 사용하여 새로운 아이템을 생성합니다.
+     *     현재는 아이템 생성 로직이 구현되어 있지 않으며, 쿼리와 요청 데이터를 통해 기존 아이템을 조회하여 반환합니다.
      *
      * @param query 아이템 생성 시 사용되는 쿼리 파라미터입니다.
-     * @param requestDto 아이템 생성에 필요한 사용자 요청 데이터를 포함한 DTO입니다.
-     * @return Item 생성된 아이템의 정보를 반환합니다.
+     * @param userRequestDto 아이템 생성에 필요한 사용자 요청 데이터를 포함하는 DTO입니다.
+     * @return Item 생성된 아이템의 정보를 반환합니다. 현재는 쿼리와 일치하는 기존 아이템을 반환합니다.
      */
-    public Item postCall(String query, UserRequestDto requestDto) {
-        // 실제 구현에서는 요청 데이터를 바탕으로 새로운 아이템을 생성하고 저장하는 로직이 들어갑니다.
-        return null; // 현재는 구현되지 않았습니다.
-    }
+    public Item postCall(String query, UserRequestDto userRequestDto) {
+        System.out.println("userRequestDto.getUsername() = " + userRequestDto.getUsername());
+        System.out.println("userRequestDto.getPassword() = " + userRequestDto.getPassword());
 
+        return getCallObject(query);
+    }
     /**
      * ✅ 클라이언트가 전달한 인증 토큰과 요청 본문 데이터를 기반으로 아이템 관련 작업을 수행합니다.
      *
